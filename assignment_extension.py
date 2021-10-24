0d19141233fc07518e7b0cd438cf4bdcae12b4f2
tanushreemandibel <86183713+tanushreemandibel@users.noreply.github.com>
2021-10-24 21:45:01 +0530

    assignment_extension.py
    assignment_extension.py

---
+++ assignment_extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 21:43:12 2021
+
+@author: TANUSHREE
+"""
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + f_extns[-1])
